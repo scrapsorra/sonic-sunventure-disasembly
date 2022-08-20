939f07648a86158600e7597aacd8c3f3b7683239
scrapsorra <fxreagan17@gmail.com>
2022-08-20 14:54:49 -0400

    advanced error handler

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @echo off
-
-IF EXIST s1built.bin move /Y s1built.bin s1built.prev.bin >NUL
-asm68k /k /p /o ae- sonic.asm, s1built.bin >errors.txt, , sonic.lst
+asm68k /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- sonic.asm, s1built.bin, sonic1.sym, sonic1.lst
+convsym sonic1.sym sonic1.symcmp
+copy /B s1built.bin+sonic1.symcmp s1built.bin /Y
+del sonic1.symcmp
+pause
 fixheadr.exe s1built.bin
