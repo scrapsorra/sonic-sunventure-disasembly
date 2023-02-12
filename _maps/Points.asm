b21b6b8044e931a5fd053a8e81e116e615ffadc2
scrapsorra <fxreagan17@gmail.com>
2023-02-12 12:23:03 -0500

    update

--- _maps/Points.asm
+++ _maps/Points.asm
@@ -2,25 +2,31 @@
 ; Sprite mappings - output from SonMapEd - Sonic 1 format
 ; --------------------------------------------------------------------------------
 
-SME_qLvsg:	
-		dc.w SME_qLvsg_E-SME_qLvsg, SME_qLvsg_14-SME_qLvsg	
-		dc.w SME_qLvsg_1A-SME_qLvsg, SME_qLvsg_20-SME_qLvsg	
-		dc.w SME_qLvsg_26-SME_qLvsg, SME_qLvsg_2C-SME_qLvsg	
-		dc.w SME_qLvsg_37-SME_qLvsg	
-SME_qLvsg_E:	dc.b 1	
-		dc.b $FC, 4, 0, 0, $F8	
-SME_qLvsg_14:	dc.b 1	
-		dc.b $FC, 4, 0, 2, $F8	
-SME_qLvsg_1A:	dc.b 1	
-		dc.b $FC, 4, 0, 4, $F8	
-SME_qLvsg_20:	dc.b 1	
-		dc.b $FC, 4, 0, 6, $F8	
-SME_qLvsg_26:	dc.b 1	
-		dc.b $FC, 0, 0, 6, $FC	
-SME_qLvsg_2C:	dc.b 2	
-		dc.b $FC, 8, 0, 6, $F4	
-		dc.b $FC, 4, 0, 7, 1	
-SME_qLvsg_37:	dc.b 2	
-		dc.b $FC, 8, 0, 6, $F4	
+SME_yDUNR:	
+		dc.w SME_yDUNR_E-SME_yDUNR, SME_yDUNR_19-SME_yDUNR	
+		dc.w SME_yDUNR_24-SME_yDUNR, SME_yDUNR_2F-SME_yDUNR	
+		dc.w SME_yDUNR_3A-SME_yDUNR, SME_yDUNR_40-SME_yDUNR	
+		dc.w SME_yDUNR_55-SME_yDUNR	
+SME_yDUNR_E:	dc.b 2	
+		dc.b $FC, 0, 0, 3, $FC	
+		dc.b $FC, 0, 0, 0, 0	
+SME_yDUNR_19:	dc.b 2	
+		dc.b $FC, 0, 0, 1, $F8	
+		dc.b $FC, 0, 0, 0, 0	
+SME_yDUNR_24:	dc.b 2	
+		dc.b $FC, 0, 0, 2, $F8	
+		dc.b $FC, 0, 0, 0, 0	
+SME_yDUNR_2F:	dc.b 2	
+		dc.b $FC, 0, 0, 3, $F8	
+		dc.b $FC, 0, 0, 0, 0	
+SME_yDUNR_3A:	dc.b 1	
+		dc.b $FC, 0, 0, 3, $FC	
+SME_yDUNR_40:	dc.b 4	
+		dc.b $FC, 4, 0, 7, 5	
+		dc.b $FC, 0, 0, 3, $F4	
+		dc.b $FC, 0, 0, 0, $F8	
+		dc.b $FC, 0, 0, 0, $FC	
+SME_yDUNR_55:	dc.b 2	
+		dc.b $FC, 8, 0, 3, $F4	
 		dc.b $FC, 4, 0, 7, 6	
 		even
